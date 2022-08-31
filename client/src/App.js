b98bd75f1280df4c14f269f15ce976afd6ed46f3
Olivia <oliviatmanalastas@gmail.com>
2022-08-31 14:40:01 -0400

    Added components and routes for about, events, and profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React , {usestate, useEffect} from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Events from './components/Events'
+import Profile from './components/Profile'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/"> HOME </Link>
+        <Link to="/about"> ABOUT </Link>
+        <Link to="/events"> EVENTS </Link>
+        <Link to="/profile"> PROFILE </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
