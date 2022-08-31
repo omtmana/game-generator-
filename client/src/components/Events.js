eb9503e8a34bf63d03264f0d0267daafb596ee04
Olivia <oliviatmanalastas@gmail.com>
2022-08-31 15:59:14 -0400

    Fetch events from rails and eventsCard component

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
+import EventsCard from './EventsCard'
 
 const Events = () => {
+   const [events, setEvents] = useState([])
+
+   useEffect(() => {
+      const getEvents = async () => {
+         let req = await fetch('http://localhost:3000/events')
+         let res = await req.json()
+         setEvents(res)
+      }
+      getEvents()
+   }, [])
+
+   console.log('events', events)
+
    return(
-      <h1> Events</h1>
+      <div>
+         {
+            events.map((gameEvents) => {
+               return <EventsCard key={gameEvents.id} gameEvents={gameEvents}/>
+            })
+         }
+      </div>
    )
 }
 
---
+++ client/src/components/EventsCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EventsCard = ({gameEvents}) => {
+   const {id, name, time} = gameEvents
+   return(
+      <div>
+         <h1>{name}</h1>
+         <h3>{time}</h3>
+      </div>
+   )
+}
+
+export default EventsCard;
