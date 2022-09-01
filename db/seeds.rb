12e185be648f5e05aebd6e3b9dd6ce1cfea473bf
Zoheb <102554873+zohebzi@users.noreply.github.com>
2022-09-01 10:16:15 -0400

    new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 
 # u1 = User.create!(
 #     name: 'Nathaniel Wolf',
-#     bio: '',
+#     bio: 'I like to play boardgames!',
 #     username: 'Nathaniel',
 #     password: '12345',
 #     email: 'nathaniel@gmail.com',
@@ -23,7 +23,7 @@
 
 # u2 = User.create!(
 #     name: 'Olivia Manalastas',
-#     bio: 'I love playing tennis!',
+#     bio: 'I like to play boardgames!',
 #     username: 'Olivia',
 #     password: '12345',
 #     email: 'olivia@gmail.com',
@@ -35,8 +35,8 @@
 
 # u3 = User.create!(
 #     name: 'Zoheb Iqbal',
-#     bio: '',
-#     username: 'amanal',
+#     bio: 'I like to play boardgames!',
+#     username: 'Zoheb',
 #     password: '12345',
 #     email: 'zoheb@gmail.com',
 #     availability: 'Anyday',
@@ -46,172 +46,193 @@
 # )
 
 # u4 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Michael Law',
+#     bio: 'I like to play boardgames!',
+#     username: 'Michael',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'michael@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble'
 #     skill_level: 'Intermediate'
 # )
 
 # u5 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Aaron Fowlkes',
+#     bio: 'I like to play boardgames!',
+#     username: 'Aaron',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'aaron@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble, Ludo'
 #     skill_level: 'Intermediate'
 # )
 
 # u6 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Ahnaf Ahmad',
+#     bio: 'I like to play boardgames!',
+#     username: 'Ahnaf',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'ahnaf@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday, Friday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble, Ludo, Jenga, Battleship, Monopoly, Risk, Connect Four, Taboo, Uno, Yahtzee'
 #     skill_level: 'Intermediate'
 # )
 
 # u7 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Antonio Arce',
+#     bio: 'I like to play boardgames!',
+#     username: 'Antonio',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'antonio@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday, Friday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble, Ludo, Jenga, Battleship, Monopoly, Risk, Connect Four, Taboo, Uno'
 #     skill_level: 'Intermediate'
 # )
 
 # u8 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Eriche Gonzales',
+#     bio: 'I like to play boardgames!',
+#     username: 'Eriche',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'eriche@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble, Ludo, Jenga, Battleship, Monopoly, Risk, Connect Four'
 #     skill_level: 'Intermediate'
 # )
 
 # u9 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'George Duncan',
+#     bio: 'I like to play boardgames!',
+#     username: 'George',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'george@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday, Thursday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble, Ludo, Jenga, Battleship, Monopoly, Risk, Connect Four, Taboo, Uno, Yahtzee'
 #     skill_level: 'Intermediate'
 # )
 
 # u10 = User.create!(
-#     name: 'Nathaniel Wolf',
-#     bio: 'I love playing tennis!',
-#     username: 'amanal',
+#     name: 'Mark Delgado',
+#     bio: 'I like to play boardgames!',
+#     username: 'Mark',
 #     password: '12345',
-#     email: 'antmana@gmail.com',
-#     availability: 'Saturday, Sunday',
-#     user_location: 'Chicago, IL'
-#     games_preference: 'Tennis'
+#     email: 'mark@gmail.com',
+#     availability: 'Monday, Tuesday, Wednesday',
+#     user_location: 'New York, NYC'
+#     games_preference: 'Chess, Cards, Scrabble'
 #     skill_level: 'Intermediate'
 # )
 
 #  l1 = Location.create!(
-#     location_name: 'Andrea\'s House',
-#     time_slots: ,
+#     location_name: 'Game house one',
+#     availabilty: 'Anyday',
 #     location_type: 'House',
-#     address: 'Somewhere in America'
+#     address: '9797 Meadowbrook Ave, New York, NYC, 10009'
 #  )
 
 #  l2 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house two',
+#     availabilty: 'Anyday',
+#     location_type: 'house',
+#     address: '277 Mammoth Ave, New York, NYC, 10011'
 #  )
 
 #  l3 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house three',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday',
+#     location_type: 'house',
+#     address: '9366 Jefferson Circle, New York, NYC, 10034'
 #  )
 
 #  l4 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house four',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday',
+#     location_type: 'game room',
+#     address: '9948 Sunbeam Lane, New York, NYC, 10024'
 #  )
 
 #  l5 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house five',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday, Friday',
+#     location_type: 'game room',
+#     address: '134 Overlook Avenue, New York, NYC, 10028'
 #  )
 
 #  l6 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house six',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday, Friday',
+#     location_type: 'school',
+#     address: '190 Green Lane, New York, NYC, 10034'
 #  )
 
 #  l7 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house seven',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday',
+#     location_type: 'school',
+#     address: '8970 Virginia Street, New York, NYC, 10027'
 #  )
 
 #  l8 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house eight',
+#     availabilty: 'Monday, Tuesday, Wednesday, Thursday',
+#     location_type: 'house',
+#     address: '63 Nicolls St, New York, NYC, 10002'
 #  )
 
 #  l9 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house nine',
+#     availabilty: 'Monday, Tuesday, Wednesday',
+#     location_type: 'hall',
+#     address: '8207 Orchard St, New York, NYC, 10016'
 #  )
 
 #  l10 = Location.create!(
-#     location_name: '?????',
-#     time_slots: '?????',
-#     location_type: '????',
-#     address: '????'
+#     location_name: 'Game house ten',
+#     availabilty: 'Monday, Tuesday, Wednesday',
+#     location_type: 'hall',
+#     address: '87 Sherman St, New York, NYC, 10032'
 #  )
 
 
 #  e1 = Event.create!(
-#     name: 'Tennis Match!'
+#     name: 'Board game day one!'
 #     location_id: l1.id,
 #     user_id: u1.id,
-#     game_name: 'Tennis Match'
+#     game_name: 'Chess'
 #  )
 
 #  e2 = Event.create!(
-#     name: '????'
+#     name: 'Board game day two'
 #     location_id: l1.id,
 #     user_id: u1.id,
-#     game_name: '????'
+#     game_name: 'Cards'
+#  )
+
+#  e3 = Event.create!(
+#     name: 'Board game day three'
+#     location_id: l1.id,
+#     user_id: u1.id,
+#     game_name: 'Scrabble'
+#  )
+
+#  e4 = Event.create!(
+#     name: 'Board game day four'
+#     location_id: l1.id,
+#     user_id: u1.id,
+#     game_name: 'Ludo'
+#  )
+
+#  e5 = Event.create!(
+#     name: 'Board game day five'
+#     location_id: l1.id,
+#     user_id: u1.id,
+#     game_name: 'Yahtzee'
 #  )
 
 # puts 'Done Seeding'
